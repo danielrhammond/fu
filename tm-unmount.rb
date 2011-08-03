8d94fe24aca3d11988b7a8e58fcf77d70c69e3ad
Daniel Hammond <daniel@twotoasters.com>
2011-08-03 12:55:23 -0400

    updated volume name

--- tm-unmount.rb
+++ tm-unmount.rb
@@ -4,7 +4,7 @@
 # the mouse
 
 mount_string = `mount`
-matches = mount_string.match(/(.+) on \/Volumes\/Time Machine/)
+matches = mount_string.match(/(.+) on \/Volumes\/Aria Backup/)
 abort("No Time Machine Volume Found") if matches.nil?
 mount = matches[1]
 exec "diskutil unmount #{mount}"
